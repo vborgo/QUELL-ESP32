8ec1bb0a3a69fc243915ff6fe3f58d4d42141cac
vitorborgo <vitorborgo@outlook.com>
2021-12-23 22:39:25 +0000

    Protocol created and functions to check and process it. CRC16-CCITT calculation function added. Everything needs testing.

--- quell/main/ProtocolTask/protocolTask.c
+++ quell/main/ProtocolTask/protocolTask.c
@@ -8,19 +8,139 @@
 #include "protocolTask.h"
 
 static const char *TAG = "protocol";
-
-
 #define EX_UART_NUM UART_NUM_1
-// #define PATTERN_CHR_NUM    (3)         /*!< Set the number of consecutive and identical characters received by receiver which defines a UART pattern*/
 
 #define BUF_SIZE (1024)
 #define RD_BUF_SIZE (BUF_SIZE)
 static QueueHandle_t uart_queue;
 
+
+#define QUELL_OK (0)
+#define QUELL_ERROR (-1)
+
+
+#define SOH 1
+#define SOT 2
+#define EOT 3
+#define MINIMUM_PACKET_SIZE 7
+
+/*
+* One of the ways to calculate CRC16
+* From: https://stackoverflow.com/questions/17196743/crc-ccitt-implementation
+*/
+static uint16_t calculateCRC16CCITT(char *ptr, int16_t count)
+{
+   int  crc;
+   char i;
+   crc = 0;
+
+   if(ptr == NULL || count == 0)
+   {
+       return 0;
+   }
+
+   while (--count >= 0)
+   {
+      crc = crc ^ (int) *ptr++ << 8;
+      i = 8;
+      do
+      {
+         if (crc & 0x8000)
+            crc = crc << 1 ^ 0x1021;
+         else
+            crc = crc << 1;
+      } while(--i);
+   }
+   return (crc);
+}
+
+static int32_t calculatePacketCRC16(uint16_t *_pu16CRC16, uint8_t *_pu8Packet, uint16_t _u16PacketSize)
+{
+    if(_pu16CRC16 == NULL || _pu8Packet == NULL || _u16PacketSize)
+    {
+        return QUELL_ERROR;
+    }
+
+    /* Calculate the CRC16, eliminating the CRC16 bytes (of course) */
+    *_pu16CRC16 = calculateCRC16CCITT((char*)_pu8Packet, _u16PacketSize - 2);
+
+    return QUELL_OK;
+}
+
+/*
+    PACKET DESCRIPTION (Big Endian)
+
+    PACKET ITEM:            LENGTH:             DESCRIPTION:                    CONST VALUE:
+    SOH                     u8                  Start of heading                0X01
+    Packet Size             u16                 Packet size                     NO
+    SOT                     u8                  Start of text                   0x02
+    Message                 Variable            Message content                 NO
+    ETX                     u8                  End of Text                     0x03
+    CRC16                   u16                 CRC16 of header + message       NO
+*/
+static int32_t verifyProtocolPacket(uint8_t *_pu8Packet, uint16_t _u16PacketSize)
+{
+    uint16_t u16CalculatedCRC16 = 0;
+
+    if(_pu8Packet == NULL || _u16PacketSize)
+    {
+        return QUELL_ERROR;
+    }
+
+    /* A packet needs at least 7 bytes to be valid */
+    if(_u16PacketSize < MINIMUM_PACKET_SIZE)
+    {
+        return QUELL_ERROR;
+    }
+
+    /* Check the const bytes (SOH, SOT and )  */
+    if(_pu8Packet[0] != SOH || _pu8Packet[3] != SOH || _pu8Packet[_u16PacketSize - 3] != EOT)
+    {
+        return QUELL_ERROR;
+    }
+
+    /*Check if the message size is correct*/
+    if(*((uint16_t*)(&_pu8Packet[1])) != _u16PacketSize - MINIMUM_PACKET_SIZE) //Maybe have created a union to get the u16 from the packet array without crazy casts would have been a good idea
+    {
+        return QUELL_ERROR;
+    }
+
+    /* Checks CRC16 */
+    if(calculatePacketCRC16(&u16CalculatedCRC16, _pu8Packet, _u16PacketSize) == QUELL_ERROR || u16CalculatedCRC16 != *((uint16_t*)(&_pu8Packet[_u16PacketSize - 2])))
+    {
+        return QUELL_ERROR;
+    }
+
+    return QUELL_OK;
+}
+
+static int32_t extractMessageFromPacket(uint8_t *_pu8Packet, uint16_t _u16PacketSize, uint8_t *_pu8Message, uint16_t *_u16MessageSize)
+{
+    if(_pu8Packet == NULL || _u16PacketSize)
+    {
+        return QUELL_ERROR;
+    }
+
+    /* A packet needs at least 7 bytes to be valid */
+    if(_u16PacketSize < MINIMUM_PACKET_SIZE)
+    {
+        return QUELL_ERROR;
+    }
+
+    /* Points to start of the message */
+    _pu8Message = &_pu8Packet[4];
+    (void)_pu8Message;
+
+    /* Gets the message size */
+    *_u16MessageSize = *((uint16_t*)(&_pu8Packet[1]));
+    
+
+    return QUELL_OK;
+}
+
 static void protocol_task(void *pvParameters)
 {
     uart_event_t event;
-    size_t buffered_size;
     uint8_t* dtmp = (uint8_t*) malloc(RD_BUF_SIZE);
     for(;;) 
     {
@@ -38,7 +158,7 @@ static void protocol_task(void *pvParameters)
                     ESP_LOGI(TAG, "[UART DATA]: %d", event.size);
                     uart_read_bytes(EX_UART_NUM, dtmp, event.size, portMAX_DELAY);
                     ESP_LOGI(TAG, "[DATA EVT]:");
-                    uart_write_bytes(EX_UART_NUM, (const char*) dtmp, event.size);
+                    //uart_write_bytes(EX_UART_NUM, (const char*) dtmp, event.size);
                     break;
                 //Event of HW FIFO overflow detected
                 case UART_FIFO_OVF:
@@ -69,25 +189,6 @@ static void protocol_task(void *pvParameters)
                 case UART_FRAME_ERR:
                     ESP_LOGI(TAG, "uart frame error");
                     break;
-                //UART_PATTERN_DET
-                // case UART_PATTERN_DET:
-                //     uart_get_buffered_data_len(EX_UART_NUM, &buffered_size);
-                //     int pos = uart_pattern_pop_pos(EX_UART_NUM);
-                //     ESP_LOGI(TAG, "[UART PATTERN DETECTED] pos: %d, buffered size: %d", pos, buffered_size);
-                //     if (pos == -1) {
-                //         // There used to be a UART_PATTERN_DET event, but the pattern position queue is full so that it can not
-                //         // record the position. We should set a larger queue size.
-                //         // As an example, we directly flush the rx buffer here.
-                //         uart_flush_input(EX_UART_NUM);
-                //     } else {
-                //         uart_read_bytes(EX_UART_NUM, dtmp, pos, 100 / portTICK_PERIOD_MS);
-                //         uint8_t pat[PATTERN_CHR_NUM + 1];
-                //         memset(pat, 0, sizeof(pat));
-                //         uart_read_bytes(EX_UART_NUM, pat, PATTERN_CHR_NUM, 100 / portTICK_PERIOD_MS);
-                //         ESP_LOGI(TAG, "read data: %s", dtmp);
-                //         ESP_LOGI(TAG, "read pat : %s", pat);
-                //     }
-                //     break;
                 //Others
                 default:
                     ESP_LOGI(TAG, "uart event type: %d", event.type);
